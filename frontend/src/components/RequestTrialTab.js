bf62a58a68c443e2e907e29d9911604444fb4d91
ICONIX <101103144+iconixgroups@users.noreply.github.com>
2023-11-03 11:06:50 +0400

    Revert "Comprehensive Code Analysis and Enhancement"

--- frontend/src/components/RequestTrialTab.js
+++ frontend/src/components/RequestTrialTab.js
@@ -1,60 +1,47 @@
-import React from "react";
+import React from 'react';
 
 class RequestTrialTab extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       product: null,
-      trialLink: null,
+      trialLink: null
     };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     // Assuming "fetchProduct" is a function that fetches the product details
-    try {
-      const product = await this.fetchProduct();
+    this.fetchProduct().then(product => {
       this.setState({
         product: product,
-        trialLink: product.trialLink,
+        trialLink: product.trialLink
       });
-    } catch (error) {
-      console.error(error);
-    }
+    });
   }
 
-  async fetchProduct() {
+  fetchProduct() {
     // Fetch product details here
     // This is a placeholder and should be replaced with actual implementation
-    const response = await fetch("https://api.example.com/product");
-    const product = await response.json();
-    return product;
+    return Promise.resolve({
+      name: 'AI Product',
+      trialLink: 'https://www.example.com/trial'
+    });
   }
+
   render() {
     const { product, trialLink } = this.state;
 
     if (!product) {
-      return (
-        <div>
-          <span>Loading...</span>
-          <span
-            className="spinner-border spinner-border-sm"
-            role="status"
-            aria-hidden="true"
-          ></span>
-        </div>
-      );
+      return <div>Loading...</div>;
     }
 
     return (
       <div id="requestTrialTab">
         <h2>{product.name} - Request Trial</h2>
         <p>
-          Interested in trying out {product.name}? Click the link below to sign
-          up for a trial.
+          Interested in trying out {product.name}? Click the link below to sign up for a trial.
         </p>
-        <a href={trialLink} target="_blank" rel="noopener noreferrer">
-          Request Trial
-        </a>
+        <a href={trialLink} target="_blank" rel="noopener noreferrer">Request Trial</a>
       </div>
     );
   }
